15de3f16da44711a141e917b5fdb087669005ec0
Kamran <Yousef.shaikhan@gmail.com>
2025-09-07 00:30:55 +0100

    added_async await_ftech
    Added async await fetch to use the API from TMDB

--- Movie-show-app/src/assets/Components/Movies/Movies.jsx
+++ Movie-show-app/src/assets/Components/Movies/Movies.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Movies.css'
 import WatchMovies from './WatchMovies'
 
 const Movies = () => {
+    //.then((res) => console.log(res.json()));
+    //.the((res) => res.json().then((data)=> console.log(data)));
+    ///////////.then
+
+    // useEffect(() => {
+    //     fetch("https://api.themoviedb.org/3/person/popular?api_key=2e329f464285ec2d95e238087927516d").then((response) => {
+
+    //         console.log(response);//you see a Response object
+    //         return response.json();//read the body as JSON
+    //     })
+    //     .then(data => {
+    //         console.log(data); // here is a Real Object such as {id; name;....}
+    //     });
+
+    // }, []);
+    //Promise = I’ll bring the data later.
+    ///Response = The server answered, here’s the envelope.
+    //data (after .json()) = Here’s the actual content inside.
+
+    /////////////////////////////////.async await: more readabale and no cunfusing
+    useEffect(() => {
+        fetchMovies();
+    }, [])
+
+    const fetchMovies = async () => {
+        const response = await fetch('https://api.themoviedb.org/3/person/popular?api_key=2e329f464285ec2d95e238087927516d');
+        const data = await response.json();
+        console.log(data);
+    }
+
+
+
     return (
         <section className="movie-list">
             <header className="movieheader">
@@ -26,7 +58,7 @@ const Movies = () => {
             </header>
 
             <div className='movie-shows'>
-                <WatchMovies/>
+                <WatchMovies />
             </div>
         </section>
     )
